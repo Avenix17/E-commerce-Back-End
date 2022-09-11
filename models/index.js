a8dff366b182976b0012167850689c63165a8f83
Avyrie Fellows <avyriefellows@gmail.com>
2022-09-10 18:09:36 -0600

    Built association methods for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id'
+});
+
 
 module.exports = {
   Product,
